2df1234fe55a956c25f9ae0e29b144c5b85c661f
nagaraju <gajjavellynagaraju@gmail.com>
2018-05-26 14:32:29 +0530

    Initial commit

---
+++ app/src/main/java/com/kkmca/kk_mca/MainActivity.java
@@ -0,0 +1,24 @@
+package com.kkmca.kk_mca;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler h=new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,NavigationBar.class));
+           finish();
+            }
+        },2000);
+    }
+}
